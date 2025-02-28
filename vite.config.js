994a1e46b3ce781964b662da62590c8a9fd9f5df
Yukuiii <472619563@qq.com>
2025-03-01 00:02:18 +0800

    build(vite): 配置 Vite 开发环境和路径别名
    - 添加 AutoImport 和 Components 插件，支持自动导入 Vue 和 Element Plus
    - 配置自动生成 auto-imports.d.ts 和 components.d.ts 类型声明文件
    - 添加 '@' 路径别名，简化模块导入路径
    - 优化 Vite 配置，提升开发体验

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    AutoImport({
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: [
+        'vue',
+        'vue-router',
+      ],
+      // 生成自动导入的类型声明文件
+      dts: 'src/auto-imports.d.ts',
+      // 自动导入目录下的模块
+      dirs: [
+        'src/composables/**',
+        'src/stores/**'
+      ],
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      // 自动导入的组件目录
+      dirs: ['src/components'],
+      // 组件的有效文件扩展名
+      extensions: ['vue'],
+      // 配置type文件生成位置
+      dts: 'src/components.d.ts',
+      // 自动导入组件
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
